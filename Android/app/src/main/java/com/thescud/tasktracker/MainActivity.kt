cce936e348d3cea1957532ba9bd1f1e6c427b63c
g.ahmeti <gent.ahmeti@deliveryhero.com>
2024-03-16 23:15:51 +0100

    Wire ui together to show task list

--- Android/app/src/main/java/com/thescud/tasktracker/MainActivity.kt
+++ Android/app/src/main/java/com/thescud/tasktracker/MainActivity.kt
@@ -3,6 +3,8 @@ package com.thescud.tasktracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.thescud.task_list_ui.TaskListScreen
 import com.thescud.tasktracker.ui.theme.TaskTrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +25,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        Greeting(name = "Task Tracker (BETA)")
+                        TaskListScreen(Modifier.fillMaxHeight())
+                    }
                 }
             }
         }
